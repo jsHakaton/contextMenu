182fbf6ae8d1119106dcf115bead0db256f18ebc
jsHakaton <js_hakaton_1@mail.ru>
2024-03-23 00:43:12 +0300

    add timer-module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import './styles.css'
 import { TestModule } from './modules/test.module'
 import { ContextMenu } from './menu'
+import { TimerModule } from './modules/timer.module'
 
-const testModule = new TestModule('text', 'TestModuleMenu')
+const testModule = new TestModule('test', 'TestModuleMenu')
+const timerModule = new TimerModule('timer', 'TimerModule')
 
 const contextMenu = new ContextMenu('.menu')
 contextMenu.add(testModule)
+contextMenu.add(timerModule)
