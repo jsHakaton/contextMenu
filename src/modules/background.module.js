3c09beb62d9d59b0f588699fbf1da1338cbe5105
jsHakaton <js_hakaton_1@mail.ru>
2024-03-23 10:19:52 +0300

    fix close menu

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -3,9 +3,7 @@ import { getRandomColor } from '../utils'
 
 export class BackgroundModule extends Module {
     trigger() {
-        document.querySelector(`[data-type=${this.type}]`).addEventListener('click', () => {
-            const body = document.querySelector('body');
-            body.style.background = `linear-gradient(${getRandomColor() }, ${getRandomColor()}, ${getRandomColor()}) no-repeat`;
-        })
+        const body = document.querySelector('body');
+        body.style.background = `linear-gradient(${getRandomColor() }, ${getRandomColor()}, ${getRandomColor()}) no-repeat`;
     }
 }
