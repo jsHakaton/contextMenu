ceb27a99ee377069999175bf38e641259f6924a1
jsHakaton <js_hakaton_1@mail.ru>
2024-03-24 16:59:56 +0300

    add more cats module

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import { TimerModule } from './modules/timer.module'
 import { BackgroundModule } from './modules/background.module'
 import { SoundModule } from './modules/sound.module'
 import { CatModule } from './modules/cat.module'
+import { MoreCatsModule } from './modules/moreCats.module'
 
 
 const timerModule = new TimerModule('timer', 'TimerModule')
 const backgroundModule = new BackgroundModule('background', 'Random background')
 const soundModule = new SoundModule('sound', 'Random sound')
 const catModule = new CatModule('cat', 'Add Cat')
+const moreCatsModule = new MoreCatsModule('moreCats', 'More Cats')
 
 
 const contextMenu = new ContextMenu('.menu')
@@ -17,3 +19,4 @@ contextMenu.add(timerModule)
 contextMenu.add(backgroundModule)
 contextMenu.add(soundModule)
 contextMenu.add(catModule)
+contextMenu.add(moreCatsModule)
---
+++ src/modules/moreCats.module.js
@@ -0,0 +1,72 @@
+import {Module} from '../core/module'
+import { random } from '../utils'
+
+
+export class MoreCatsModule extends Module {
+    constructor(type, text) {
+        super(type, text)
+    }
+
+    trigger() {
+        const container = document.querySelector('.image-container')
+        if(container){
+            container.remove()
+        }
+
+        const imageContainer = document.createElement('div')
+        imageContainer.classList.add('image-container')
+        imageContainer.style.display = 'flex'
+        imageContainer.style.flexWrap = 'wrap'
+        imageContainer.style.zIndex = '2'
+        imageContainer.style.justifyItems = 'center'
+        imageContainer.style.alignItems = 'center'
+        document.body.appendChild(imageContainer)
+
+
+        const x = window.innerWidth - 100
+        const y = window.innerHeight
+        const countColums = Math.trunc(x / 100)
+        const countRows = Math.trunc(y / 100)
+
+        console.log(x, y)
+        console.log(countColums, countRows)
+        
+        for (let i = 0; i < countRows; i++) {
+                console.log('New string', i)
+            for (let j = 0; j < countColums; j++) {
+
+                const catNumber = random(1,9)
+                const img = document.createElement('img');
+                img.src = `../../assets/images/cat_${catNumber}.svg`;
+                img.alt = 'Кот';
+                img.style.width = '100px'
+                img.style.height = '100px'
+                imageContainer.append(img);
+
+            }
+        }
+
+        // function createImageWithDelay(catNumber) {
+        //     const img = document.createElement('img');
+        //     img.src = `../../assets/images/cat_${catNumber}.svg`;
+        //     img.alt = 'Кот';
+        //     img.style.width = '100px';
+        //     img.style.height = '100px';
+        //     imageContainer.append(img);
+        // }
+        
+        // for (let i = 0; i < countRows; i++) {
+        //     console.log('New string', i);
+        //     setTimeout(() => {
+        //         for (let j = 0; j < countColums; j++) {
+        //             const catNumber = random(1, 9);
+        //             setTimeout(createImageWithDelay.bind(null, catNumber), j * 1000); // Задержка в 1 секунду между появлением картинок в одной строке
+        //         }
+        //     }, i * 1000); // Задержка в 1 секунду перед добавлением новой строки
+        // }
+
+        // setTimeout(() => {
+            // img.remove()
+        // }, 10000);
+    }
+}
